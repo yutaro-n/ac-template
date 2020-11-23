91ad3f2a02861901364cc8cc3abd29cc68a84078
Yutaro Nagasawa <yutaro.ngsw@gmail.com>
2020-11-23 16:42:16 +0900

    use gocopy and gopaste in linux

--- done.sh
+++ done.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/sh
 set -eu
 
+
+# please install gocopy
+# go get github.com/atotto/clipboard/cmd/gocopy
+# if you use wsl you install windosw side and set WSLENV USERPROFILE/up
 if [ -n $WSLENV ]; then
     cat src/main.rs | $USERPROFILE/go/bin/gocopy.exe
 else
-    cat src/main.rs | xsel -bi
+    cat src/main.rs | gocopy
 fi
 
 name="src/$1.rs"
--- ran.sh
+++ ran.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/sh
 set -eu
 
+# please install gopaste
+# go get github.com/atotto/clipboard/cmd/gopaste
+# if you use wsl you install windosw side and set WSLENV USERPROFILE/up
 if [ -n $WSLENV ]; then
     $USERPROFILE/go/bin/gopaste.exe | cargo run
 else
-    xclip -o -selection clipboard | cargo run
+    gopaste | cargo run
 fi
