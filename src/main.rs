83858a472b697b5727a6fa15bce1457643ae5db2
Yutaro Nagasawa <yutaro.ngsw@gmail.com>
2024-02-13 23:44:28 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![allow(unused_imports)]
+use std::collections::*; //{Vec, VecDeque, LinkedList, HashMap, BTreeMap, HashSet, BTreeSet, BinaryHeap};
 use itertools::Itertools;
 use proconio::{
     input,
-    marker::{Chars, Bytes, Usize1}
+    marker::* //{Chars, Bytes, Isize1, Usize1}
 };
 
 fn main() {
